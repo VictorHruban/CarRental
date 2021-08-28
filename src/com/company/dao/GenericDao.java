c5c58365daf7232f2b52d5c9b278cf880d3c1980
Victor .H <islipuknot@gmail.com>
2021-08-28 19:32:00 +0300

    changed generic dao

--- src/com/company/dao/GenericDao.java
+++ src/com/company/dao/GenericDao.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public abstract class GenericDao {
-    protected final String connectionUrl = "jdbc:mysql://localhost:3306/carRental";
-    protected final String user = "root";
-    protected final String password= "victor";
+    protected final String connectionUrl = "your url";
+    protected final String user = "user";
+    protected final String password= "password";
 
     protected Connection getConnection() throws SQLException {
         return DriverManager.getConnection(connectionUrl,user,password);
